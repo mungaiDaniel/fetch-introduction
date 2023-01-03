c53221ad05767aec362d7433b461c98279541965
mungaiDaniel <danitomonga@gmail.com>
2023-01-03 17:43:27 +0300

    users.json

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getUser').addEventListener('click', getUsers);
 
-function getText(){
+function getText() {
     fetch('sample.txt')
-    .then(function(res){
-        return res.text();
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById('output').innerHTML = data;
     })
-    .then(function(data){
-        console.log(data);
+    .catch((err) => console.log(err))
+}
+
+function getUsers(){
+    fetch('users.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '<h2>Users</h2>';
+        data.forEach(function(user){
+            output += `
+            <ul class="list-group mb-3">
+              <li class="list-group-item">ID: ${user.id}</li>
+              <li class="list-group-item">Name: ${user.name}</li>
+              <li class="list-group-item">Email: ${user.email}</li>
+            </ul>
+          `;
+        });
+        document.getElementById('output').innerHTML = output;
     })
 }
